9e870955c1da9df8ad87b6fe6b0ded90e7dc1865
Jake Troughton <jaketroughton94@gmail.com>
2020-08-11 21:38:30 +1000

    Add main camera singleton

---
+++ Assets/_Project/Scripts/MainCamera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : Singleton<MainCamera>
+{
+    public new Camera camera;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        if (!camera)
+        {
+            camera = GetComponent<Camera>();
+        }
+    }
+
+    public Vector2 WorldToViewport(Vector2 position)
+    {
+        return camera.WorldToViewportPoint(position);
+    }
+
+    public Vector2 ViewportToWorld(Vector2 position)
+    {
+        return camera.ViewportToWorldPoint(position);
+    }
+
+    public bool IsOnScreen(Vector2 position)
+    {
+        Vector2 vPos = WorldToViewport(position);
+        return vPos.x >= 0f && vPos.x <= 1f && vPos.y >= 0f && vPos.y <= 1f;
+    }
+}
